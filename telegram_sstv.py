078f5536d58b796a8bfba23175586f6c67b61435
Mari Kunagisa <git@kunagisa.moe>
2020-02-22 05:51:58 +0800

    Init project.

--- telegram_sstv.py
+++ telegram_sstv.py
@@ -1,5 +1,30 @@
 '''
-Documentation, License etc.
+This is a simple bot to use telegram API to create and read SSTV voice signal.
 
 @package telegram_sstv
 '''
+
+import pysstv as sstv
+import telebot
+
+from config import *
+
+
+# Init bots.
+## Enable proxy if configured
+if(bot_enable_proxy):
+    telebot.apihelper.proxy = bot_proxy
+
+bot = telebot.TeleBot(bot_api_token, num_threads=bot_threads_num)
+
+# Help info
+@bot.message_handler(content_types=['text'], commands=['start', 'help'])
+def send_hello(message):
+    bot.reply_to(message, "Send me picture then I'll convert it to a SSTV video file.")
+
+# Encode picture
+@bot.message_handler(content_types=['photo'])
+def sstv_encode(message):
+    print(message.photo)
+
+bot.polling()
