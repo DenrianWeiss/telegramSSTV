60f1dbc4ad2691d8995a3bee6072fc4646e5f54d
Mari Kunagisa <git@kunagisa.moe>
2020-02-22 08:33:45 +0800

    Make it works(with an ugly workaround)

--- telegram_sstv.py
+++ telegram_sstv.py
@@ -4,8 +4,12 @@ This is a simple bot to use telegram API to create and read SSTV voice signal.
 @package telegram_sstv
 '''
 
-import pysstv as sstv
+import pysstv.color as sstvutil
 import telebot
+import requests
+import os
+from io import BytesIO
+from PIL import Image, ImageOps
 
 from config import *
 
@@ -17,6 +21,20 @@ if(bot_enable_proxy):
 
 bot = telebot.TeleBot(bot_api_token, num_threads=bot_threads_num)
 
+# Resize Image
+def resize_img_with_padding(picture: Image, width: int, height: int) -> Image:
+    original_size = picture.size
+    ratio = min(float(width)/picture.width, float(height)/picture.height)
+    new_size = tuple([int(x*ratio) for x in original_size])
+    resized = Image.new("RGB", (width, height))
+    resized.paste(picture.resize(new_size, Image.ANTIALIAS), ((width-picture.width)//2, (width-picture.width)//2))
+    return resized
+
+# SSTV helper function
+def sstv_convert(picture: Image):
+    sstv = sstvutil.Robot36(resize_img_with_padding(picture, 320, 240), 48000, 16)
+    sstv.write_wav('tmp/sstvbot/114514.wav')
+
 # Help info
 @bot.message_handler(content_types=['text'], commands=['start', 'help'])
 def send_hello(message):
@@ -25,6 +43,15 @@ def send_hello(message):
 # Encode picture
 @bot.message_handler(content_types=['photo'])
 def sstv_encode(message):
-    print(message.photo)
+    # Get file from message
+    file_url = bot.get_file_url(message.photo[0].file_id)
+    received_img = Image.open(BytesIO(requests.get(file_url).content))
 
+    sstv_convert(received_img)
+    bot.send_document(message.chat.id, 'tmp/sstvbot/114514.wav' , reply_to_message_id = message.message_id)
+    os.remove('tmp/sstvbot/114514.wav')
+    received_img.close()
+
+os.remove('tmp/sstvbot')
+os.mkdir('tmp')
 bot.polling()
